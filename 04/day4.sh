4fe29f6bec8633f3b6ed3dd3764d3ead90d3ec15
marianne.bjoroen@hafslundeco.no <marianne.bjoroen@hafslundeco.no>
2022-12-04 09:31:27 +0100

    Removed printing, updated echo

--- 04/day4.sh
+++ 04/day4.sh
@@ -8,8 +8,8 @@ while IFS='-,' read -r a b c d
 do
     if [[ $a -le $c && $b -ge $d ]] || [[ $c -le $a && $d -ge $b ]]
     then
-        echo "$a $b $c $d"
+        #echo "$a $b $c $d"
         ((counter++))
     fi
 done < "$input_file"
-echo "$counter"
+echo "Star 1: fully enclosed ranges: $counter"
