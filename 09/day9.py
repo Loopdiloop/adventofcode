6b2ad8f2fb3d16a0ccea3c3f3e3e099e86e6514f
marianne.bjoroen@hafslundeco.no <marianne.bjoroen@hafslundeco.no>
2022-12-14 14:49:53 +0100

    Updated day9 for star 2 as well. Struggled with a factor 2 for a long time...

--- 09/day9.py
+++ 09/day9.py
@@ -1,7 +1,19 @@
 import numpy as np
+import sys
 
-H=np.array([0,0], dtype=np.int64)
-T=np.array([0,0], dtype=np.int64)
+try:
+    star = int(sys.argv[1])
+    assert star in [1,2]
+except:
+    star = 2
+
+if star == 1:
+    n = 2
+elif star == 2:
+    n = 10
+
+#KNOTS.
+K=np.zeros((n+2,2), dtype=np.int64)
 previous_positions = []
 
 movements = dict(R = np.array([1,0]),
@@ -10,46 +22,49 @@ movements = dict(R = np.array([1,0]),
     D = np.array([0,-1]),
 )
 
-def stable(H,T):
-    diff = H-T
-    if all(np.absolute(diff) <= np.ones(2)) == True:
+def stable(I_stable,J_stable): # IJ / HT doesn't matter
+    diff_stable = I_stable - J_stable
+    if all(np.absolute(diff_stable) <= np.ones(2)) == True:
         return True
     else:
         return False
 
-def update_T(H,T):
-    diff = H-T
+def update_T(I,J): #I head, J tail
+    diff = I-J
     abs_diff = list(np.absolute(diff))
     if [0] in abs_diff and [2] in abs_diff:
-        T += (diff/2).astype(int)
+        J += (diff/2).astype(int)
 
     elif abs_diff == [1,2] or abs_diff == [2,1]:
-        T += (diff/np.absolute(diff)).astype(np.int64)
+        J += (diff/np.absolute(diff)).astype(np.int64)
     
+    elif abs_diff == [2,2]:
+        J += (diff/np.absolute(diff)).astype(np.int64)
     else:
-        print("ERROR?! ", H, T, diff)
+        print("ERROR?! ", "pulling", I, " following", J, "diff is:", diff)
         assert False
-    return T
+    return J
 
 with open("input.txt") as f:
     for line in f:
         l = line.strip("\n")
         l = l.split(" ")
+
         for i in range(int(l[1])):
-            H += movements[l[0]]
-            if not stable(H,T):
-                T = update_T(H,T)
-            T_pos = str(T[0])+":"+str(T[1])
-            if T_pos not in previous_positions:
-                previous_positions.append(T_pos)
+            K[0] += movements[l[0]]
+            for knot in range(1,n): #Move the rest of the knots after K[0]
+                if not stable(K[knot-1], K[knot]):
+                    K[knot] = update_T(K[knot-1], K[knot])
+            
+            tail_pos = str(K[n-1][0])+":"+str(K[n-1][1])
+            if tail_pos not in previous_positions:
+                previous_positions.append(tail_pos)
 
-previous_positions.sort()
-for element in previous_positions:
-    print(element)
-print("Previous positions: ", len(previous_positions))
-
+print("Star %d, number of previous positions for tail: "%star, len(previous_positions))
 
 
+
+# For sanity:
 def test_update_T():
     test1 = update_T(np.array([4,4]), np.array([4,6]))
     assert all(test1 == np.array([4,5])), test1
@@ -60,7 +75,6 @@ def test_update_T():
     test3 = update_T(np.array([3,-7]), np.array([2,-9]))
     assert all(test3 == np.array([3,-8])), test3
     print("Test update ok.")
-
 
 def test_stable():
     assert stable(np.array([4,4]), np.array([5,4]))
